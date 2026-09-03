4f56a8620e5f28f22e0c13b013852155f31314b0
BloYoMind <grady.jeremiah.smith@gmail.com>
2026-09-03 15:05:25 -0500

    Add project files.

---
+++ LethalWeights/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine; 
+
+namespace LethalWeights.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void LethalWeightsPatch(PlayerControllerB __instance)
+        {
+            
+        }
+    }
+}
